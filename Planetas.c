a28ba829e2cb680095a5aa923ea62bd10ff9d793
2FelixCastro6 <ubuntu@2FelixCastro6>
2017-07-17 16:39:39 -0500

    inicializacion matrices necesarias para el ejecicio

--- Planetas.c
+++ Planetas.c
@@ -2,23 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 int main(void){
-	FILE *file:
-	file=fopen("coordinates(2).cvs","r");
-	int len=250;
-	char line_buffer[len];
-	const char delimiter;
-	delimiter:",";
-	while(fgets(line_buffer, len, file)){
-		printf("LINE IS: %s" line_buffer);
-		split_buffer=strlok(line_buffer, delimiter);
-		while(split_buffer !=null){
-			//matriz[i][j]
-			printf("ITEM IN LINE %s\n" split_buffer);
-			split_buffer=strlok(null,delimiter);
-						
-		}
+/*FILE *file:
+file=fopen("coordinates(2).cvs","r");
+int len=250;
+char line_buffer[len];
+const char delimiter;
+delimiter=",";
 
+while(fgets(line_buffer, len, file)){
+	printf("LINE IS: %s" line_buffer);
+	split_buffer=strlok(line_buffer, delimiter);
+	while(split_buffer !=null){
+		//matriz[i][j]
+		printf("ITEM IN LINE %s\n" split_buffer);
+		split_buffer=strlok(null,delimiter);
+						
 	}
-return 0;
 
 }
+*/	
+float *Vmediasx= malloc(10*sizeof(float));
+Vmediasx[0]=1;
+return Vmediasx;
+float *Vmediasy= malloc(10*sizeof(float));
+Vmediasy[0]=1;
+return Vmediasy;
+float *Vmediasz= malloc(10*sizeof(float));
+Vmediasz[0]=1;
+return Vmediasz;
+float **X= malloc(10*sizeof(float*));
+float **Y= malloc(10*sizeof(float*));
+float **Z= malloc(10*sizeof(float*));
+float **Vx= malloc(10*sizeof(float*));
+float **Vy= malloc(10*sizeof(float*));
+float **Vz= malloc(10*sizeof(float*));
+
+
+
+return 0;
+
+}                                                                                                                                        
