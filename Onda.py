70d9998565ed3b1117777ccde409c57a11aa0b0b
2FelixCastro6 <felix@ubuntu>
2017-07-20 12:53:11 -0500

    se anade una iteracion mas para obtener el tiempo n-1 y se simplifica la ecuacion

--- Onda.py
+++ Onda.py
@@ -1,28 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-pasadoU = np.zeros((30,30))
-presenteU = np.zeros((30,30))
-futuroU = np.zeros((30,30))
+points=300
+pasadoU = np.zeros((points,points))
+presenteU = np.zeros((points,points))
+futuroU = np.zeros((points,points))
+mascarilla=np.ones((points,points))
 tiempos=[]
-presenteU[10,15]=-0.5
-delta_x=30./30.
-delta_y=30./300.
+presenteU[100,150]=-0.5
+delta=30./puntos
 c=1
 delta_t=c*0.5*delta_x
-n=60./delta_t
-k=delta_t
-while k<=60:
-	for i in range(1,len(presenteU)-1):
+#n=60./delta_t
+for i in range(1,len(presenteU)-1):
 		for j in range(1,len(presenteU)-1):
-			futuroU[i,j]=((c**2)*(delta_t**2)*(((presenteU[i-1,j]-2*presenteU[i,j]+presenteU[i+1,j])/(delta_x**2))+((presenteU[i,j-1]-2*presenteU[i,j]+presenteU[i,j+1])/(delta_y**2))))-pasadoU[i,j]-presenteU[i,j]
+			futuroU[i,j]=((c**2)*(delta_t**2)*(presenteU[i-1,j]-4*presenteU[i,j]+presenteU[i+1,j]+presenteU[i,j-1]+presenteU[i,j+1])/(delta**2))-pasadoU[i,j]-2*presenteU[i,j]
 	pasadoU=presenteU.copy()
 	presenteU=futuroU.copy()
-	tiempos.append(pasadoU)
+k=delta_t
+while k<=60.:
+	for i in range(1,len(presenteU)-1):
+		for j in range(1,len(presenteU)-1):
+			futuroU[i,j]=((c**2)*(delta_t**2)*(presenteU[i-1,j]-4*presenteU[i,j]+presenteU[i+1,j]+presenteU[i,j-1]+presenteU[i,j+1])/(delta**2))-pasadoU[i,j]-2*presenteU[i,j]
+	pasadoU=presenteU.copy()
+	presenteU=futuroU.copy()
+	tiempos.append(futuroU)
 	k+=delta_t
 
 
-plt.imshow(tiempos)
+plt.imshow(tiempos[-1])
 plt.savefig("t60.pdf")
 plt.close()
 
